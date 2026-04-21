9cdf5b393b64a4cb1c6866a27f01841b1c991ead
JAMBROONG <lanaemel19@gmail.com>
2026-04-21 14:56:02 +0700

    p

--- fix-jsx.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/app/profile/page.tsx', 'utf8');
-content = content.replace("    </div>\n    </>\n  )\n}", "    </div>\n  )\n}");
-fs.writeFileSync('src/app/profile/page.tsx', content, 'utf8');
